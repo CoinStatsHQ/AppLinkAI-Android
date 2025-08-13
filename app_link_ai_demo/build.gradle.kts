0b6e2dc731228e73addb8ac10fac8a0c5b4a3995
vahan16 <mambreyan.vahan16@gmail.com>
2025-08-13 11:19:51 +0400

    chore: update version to 1.0.7 in build files and README.md

--- app_link_ai_demo/build.gradle.kts
+++ app_link_ai_demo/build.gradle.kts
@@ -22,20 +22,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation("com.github.CoinStatsHQ:AppLinkAI-Android:1.0.6")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("com.github.CoinStatsHQ:AppLinkAI-Android:1.0.7")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
 
